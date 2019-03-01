f80af42968e0f55d11d1517de6f2f942311e4e48
Bright Olayinka <explorebright@gmail.com>
2019-03-01 20:04:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  return Object.assign({}, object [{key}: value]);
+  return Object.assign({}, object, {[key]: value})
 }
+
+
 
 
 
