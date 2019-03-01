b4ce46e5ddc90134dc95cbeffd21e51db883051d
Bright Olayinka <explorebright@gmail.com>
2019-03-01 20:13:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,11 +9,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 
-function destructivelyDeleteFromObjectByKey(object, key){
-  newObj = Object.assign({}, object);
-  delete newObj.key;
-  return newObj
-}
 
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
