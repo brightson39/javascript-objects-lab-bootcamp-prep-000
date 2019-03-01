52875cad88a152576ce7b0bd9264ba2e15ec1d42
Bright Olayinka <explorebright@gmail.com>
2019-03-01 20:15:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,26 @@ var recipes = {}
   delete object.key;
   return object;
 }
+
+var recipes = new Object();
+
+ function updateObjectWithKeyAndValue(object, key, value){
+  return Object.assign({},object,{[key]: value})
+}
+
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+ object[key] = value
+return object
+}
+
+ function deleteFromObjectByKey(object, key){
+  var newObj =  Object.assign({},object)
+  delete newObj[key]
+  return newObj
+
+ }
+
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key]
+  return object
+}
